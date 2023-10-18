28b492cbc2ccbbc7c7d0f8655a87b5f07c70ce42
Mel-TB <melinda.trami@live.fr>
2023-10-18 15:42:54 +0200

    fix(cors): Resolve CORS error when connecting to Socket.IO server
    This commit fixes a CORS error that was preventing proper communication with the Socket.IO server. The issue was related to cross-origin requests.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const server = require("http").createServer();
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credential: true,
+  },
+});
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
