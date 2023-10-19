6d333f218e809c276b57a8c4296f0c66a5c093a4
Mel-TB <melinda.trami@live.fr>
2023-10-19 14:34:03 +0200

    feat(socket-io): Implement room creation
    - Created rooms and namespace for Socket.IO connections. Rooms provide a way to group clients for the multiplayer game sessions.

--- sockets.js
+++ sockets.js
@@ -2,30 +2,39 @@
 let readyPlayerCount = 0;
 
 function listen(io) {
-  io.on("connection", (socket) => {
+  const pongNamespace = io.of("/pong");
+
+  pongNamespace.on("connection", (socket) => {
+    let roomId = "room ";
     let clientId = socket.id;
+
     console.log(`Client connected ${clientId}`);
 
     socket.on("ready", () => {
-      console.log(`Player ready ${clientId}`);
+      roomId = roomId + Math.floor(readyPlayerCount / 2);
+      socket.join(roomId);
+
+      console.log(`Player ready ${clientId}, in ${roomId}`);
 
       readyPlayerCount++;
 
       if (readyPlayerCount % 2 === 0) {
-        io.emit("startGame", clientId);
+        //
+        pongNamespace.to(roomId).emit("startGame", clientId);
       }
     });
 
     socket.on("paddleMove", (paddleData) => {
-      socket.broadcast.emit("paddleMove", paddleData);
+      socket.to(roomId).emit("paddleMove", paddleData);
     });
 
     socket.on("ballMove", (ballData) => {
-      socket.broadcast.emit("ballMove", ballData);
+      socket.to(roomId).emit("ballMove", ballData);
     });
 
     socket.on("disconnect", (reason) => {
       console.log(`Client ${clientId} disconnected: ${reason}`);
+      socket.leave(roomId);
     });
   });
 }
